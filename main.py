a6d4c3891d3aede1e914eb7609f987c29fb638c9
Castillo, David (Student) <jc797@students.waikato.ac.nz>
2026-01-21 21:47:01 +1300

    Completed update subparser/command. Need to handle errors but will work on later. Specifically when outside of list range, too many args, or incorrect index (maybe). Next is delete which shouldn't be too difficult.

--- main.py
+++ main.py
@@ -15,6 +15,14 @@ subparsers = parser.add_subparsers(dest="command")
 add_parser = subparsers.add_parser("add")
 add_parser.add_argument("description")
 
+update_parser = subparsers.add_parser("update")
+update_parser.add_argument("id", type=int)
+update_parser.add_argument("description")
+
+delete_parser = subparsers.add_parser("delete")
+delete_parser.add_argument("id", type=int)
+delete_parser.add_argument("description")
+
 # Collect parsed items and store as namespace
 args = parser.parse_args()
 
@@ -40,7 +48,7 @@ if args.command == "add":
         if task_list:
             next_id = max(task["id"] for task in task_list) + 1
         else:
-            next_id = 1
+            next_id = 0
 
         # Creating new task to add
         new_task = {
@@ -58,9 +66,34 @@ if args.command == "add":
     with open(path, 'w') as f:
         # Overwrite JSON file with updates
         json.dump(task_list, f)
-        print(f"Task added succesfully (ID: {next_id})")
+        print(f"Task added successfully (ID: {next_id})")
 
 
 
+if args.command == "update":
 
+    # Check if the JSON tasklist file exists, if not create an empty one
+    if os.path.exists(path):
+
+        print(f"'{path}' exists, accessing...")
+
+        # Open the tasklist on read mode
+        with open(path, 'r') as f:
+            # loading the JSON file into an accessible copy variable
+            task_list = json.load(f)
+
+            task_id = args.id
+
+            for item in task_list:
+                if item["id"] == task_id:
+                    print("THE ID WE'RE LOOKING AT IS " + str(item["id"]))
+                    updated_item = task_list[task_id]
+                    updated_item["description"] = args.description
+                    updated_item["updatedAt"] = datetime.datetime.now().isoformat()
+
+        with open(path, 'w') as f:
+            json.dump(task_list, f)
+            print(f"Task {task_id} has been updated: \n''{updated_item}''\n")
+    else:
+        print(f"'{path}' does not exist, no tasks to update...")
 
