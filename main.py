ba28d8c5ddc9b02315185929d9af9865e1950151
Castillo, David (Student) <jc797@students.waikato.ac.nz>
2026-01-21 18:15:03 +1300

    Add implemented. Will create functions later when I have a better understanding of how everything works together.

--- main.py
+++ main.py
@@ -1,30 +1,66 @@
 import argparse
+import datetime
 import json
 import os
 
 path = 'tasklist.json'
-if os.path.exists(path):
-    print(f"'{path}' exists, accessing...")
-else:
-    print(f"'{path}' does not exist, creating...")
-
 
 ### BASIC CLI STRUCTURE ####################################
 
 # Create parser
 parser = argparse.ArgumentParser()
+subparsers = parser.add_subparsers(dest="command")
 
-# Defining args we want to collect
-parser.add_argument("action")
-parser.add_argument("data1")
-parser.add_argument("data2")
+## Subparsers ####
+add_parser = subparsers.add_parser("add")
+add_parser.add_argument("description")
 
-# Actually get arguments to parse
+# Collect parsed items and store as namespace
 args = parser.parse_args()
 
-verb = args.action
-noun1 = args.data1
-noun2 = args.data2
 
-print(f"I want to {verb} {noun1} and {noun2}")
+### COMMANDS ################################################
+
+if args.command == "add":
+
+    # Check if the JSON tasklist file exists, if not create an empty one
+    if not os.path.exists(path):
+        print(f"'{path}' does not exist, creating...")
+        with open(path, 'w') as f:
+            json.dump([], f)
+
+    print(f"'{path}' exists, accessing...")
+
+    # Open the tasklist on read mode
+    with open(path, 'r') as f:
+        # loading the JSON file into an accessible copy variable
+        task_list = json.load(f)
+
+        # Calculating next id
+        if task_list:
+            next_id = max(task["id"] for task in task_list) + 1
+        else:
+            next_id = 1
+
+        # Creating new task to add
+        new_task = {
+            "id": next_id,
+            "description": args.description,
+            "status": "todo",
+            "createdAt": datetime.datetime.now().isoformat(),
+            "updatedAt": datetime.datetime.now().isoformat(),
+        }
+
+        # Adding new task to the tasklist copy
+        task_list.append(new_task)
+
+    # Opening tasklist but now in write mode
+    with open(path, 'w') as f:
+        # Overwrite JSON file with updates
+        json.dump(task_list, f)
+        print(f"Task added succesfully (ID: {next_id})")
+
+
+
+
 
